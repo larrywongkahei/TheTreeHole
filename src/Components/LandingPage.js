5b3876780acfed7b2fc9ebbf75ed01592914e7ac
Maryhei <makemak123@gmail.com>
2023-02-24 11:30:24 +0000

    Back to before

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 function LandingPage ({handlePlaying}) {
     return (
-        <Link to="/TheTreeHole/query" className="text-white">
+        <Link to="/TheTreeHole/query">
             Add some image and logo here
         <button onClick={handlePlaying}>Get in</button>
         </Link>
