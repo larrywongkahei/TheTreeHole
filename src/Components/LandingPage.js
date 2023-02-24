94d7263be8d509bff61936a0801205af5fcbe606
Maryhei <makemak123@gmail.com>
2023-02-24 15:12:40 +0000

    Fixed not fetching bug

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 function LandingPage ({handlePlaying}) {
     return (
-        <Link to="/query">
+        <Link to="/query" className="text-white">
             Add some image and logo here
         <button onClick={handlePlaying}>Get in</button>
         </Link>
