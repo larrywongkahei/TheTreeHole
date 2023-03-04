9e092187485832977f37a018b6f63d6278653628
Maryhei <makemak123@gmail.com>
2023-03-04 23:16:13 +0000

    Modified to bigger text size

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -3,7 +3,7 @@ import Words from "../videos/TreeHole.svg"
 
 function LandingPage ({ handlePlaying }) {
     return (
-        <Link to="/query" className="text-black text-center text-2xl mt-16">
+        <Link to="/query" className="text-gray-700 font-serif text-center text-4xl mt-16">
         <button onClick={handlePlaying}>Welcome to the <img src={Words} /></button>
         </Link>
     )
