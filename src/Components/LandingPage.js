86b9ecdb6e34f0a29c02c5ecacc21181360d9ba5
Maryhei <makemak123@gmail.com>
2023-03-04 21:20:10 +0000

    Added treehole animation

--- src/Components/LandingPage.js
+++ src/Components/LandingPage.js
@@ -1,9 +1,10 @@
 import { Link } from "react-router-dom"
+import Words from "../videos/TreeHole.svg"
 
 function LandingPage ({ handlePlaying }) {
     return (
-        <Link to="/query" className="text-black text-center text-2xl">
-        <button onClick={handlePlaying}>Get in</button>
+        <Link to="/query" className="text-black text-center text-2xl mt-16">
+        <button onClick={handlePlaying}>Welcome to the <img src={Words} /></button>
         </Link>
     )
 }
