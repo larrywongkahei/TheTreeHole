9d1fd0f140f0e3fa275d12ba573c8609e9e2c36b
Maryhei <makemak123@gmail.com>
2023-03-10 15:32:01 +0000

    Resposive page designing

--- src/Components/Query.js
+++ src/Components/Query.js
@@ -3,24 +3,24 @@ import query from "../videos/query.png"
 
 const Query = () =>{
     return (
-        <div className="text-white text-center my-auto text-5xl sm:mt-60">
+        <div className="text-white text-center my-auto text-5xl sm:mt-60 mt-40">
             <img src={query} className="pb-18"
                           style={{
-                            height: "130%",
+                            height: "110%",
                             width: "100%",
                             top: "0",
                             right: "0",
-                            padding: "none",
+                            paddingBottom:"50px",
                             position: "fixed",
                             zIndex: "-1",
                             objectFit:"cover",
                           }}/>
-            <h1 className="mb-16 text-4xl text-black">Are You the first time here?</h1>
+            <h1 className="mb-16 text-2xl font-bold text-black mx-18 mt-2">Are You the first time here?</h1>
             <Link to="/introduction">
-                <button className="bg-green-500 h-24 w-40 rounded-xl mr-5">Yes</button>
+                <button className="bg-green-500 h-12 w-20 rounded-xl mr-5 sm:h-24 sm:w-40 text-xl font-bold sm:text-5xl sm:font-normal">Yes</button>
             </Link>
             <Link to="/Signin">
-                <button className="bg-red-500 h-24 w-40 rounded-xl ml-5 mb-14">No</button>
+                <button className="bg-red-500 h-12 w-20 rounded-xl ml-5 mb-14 sm:h-24 sm:w-40 text-xl font-bold sm:text-5xl sm:font-normal">No</button>
             </Link>
 
         </div>
